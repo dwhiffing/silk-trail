62e225bfb074001f27c0d987190516ab7284f9c7
Daniel Whiffing <danielwhiffing@gmail.com>
2023-09-11 23:17:41 -0400

    Adds player sprite

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -1,13 +1,14 @@
 import { emit, onKey, onPointer } from 'kontra'
 import * as constants from '../constants'
+import { BODY_PATH, FACE_PATH } from './enemies'
 import { Sprite } from './sprite'
 import { Trajectory } from './Trajectory'
 
 export const Player = ({ canvas, data, bullets, particles, enemies }) => {
   let sprite = new PlayerSprite({
-    x: canvas.width - 70,
+    x: canvas.width - 60,
     y: constants.GROUND_Y - constants.SIZE / 3,
-    color: '#666',
+    color: 'white',
     width: constants.SIZE,
     height: constants.SIZE,
     health: constants.MAX_HP,
@@ -19,8 +20,8 @@ export const Player = ({ canvas, data, bullets, particles, enemies }) => {
   sprite.itemIndex = 0
   sprite.progress = 0
   sprite.data = data
-  const _x = () => sprite.x - constants.SIZE * 0.5
-  const _y = () => sprite.y - constants.SIZE * 1.5
+  const _x = () => sprite.x - constants.SIZE * 0.4
+  const _y = () => sprite.y - constants.SIZE * 1.2
   let item
   let updateTrajectory = (_angle, _speed) => {
     angle = _angle
@@ -74,9 +75,9 @@ export const Player = ({ canvas, data, bullets, particles, enemies }) => {
     canBlock = false
     emit('delay', 'block', 20, () => {
       sprite.block = false
-      sprite.color = '#333'
+      sprite.color = 'white'
       emit('delay', 'allow-block', 60, () => {
-        sprite.color = '#666'
+        sprite.color = 'white'
         canBlock = true
       })
     })
@@ -84,8 +85,11 @@ export const Player = ({ canvas, data, bullets, particles, enemies }) => {
 
   const onSwap = () => {
     if (trajectory.stage !== 0) trajectory.stage = 0
-    sprite.itemIndex++
-    if (sprite.itemIndex > data.items.length - 1) sprite.itemIndex = 0
+    if (item) {
+      data.items.push(item.type)
+      item.ttl = 0
+    }
+    getItem()
   }
 
   onKey('space', (e) => !e.repeat && onBlock())
@@ -151,6 +155,12 @@ class PlayerSprite extends Sprite {
 
     const s = constants.SIZE / 2
     this.context.translate(s, s * 2)
+    this.context.scale(-1, 1)
+    this.context.rotate(0.2)
+    this.drawPath(BODY_PATH, '#B6AC88', this.color, 1, -150, -150)
+    this.drawPath(FACE_PATH, '#B6AC88', '#DEA248', 1, -150, -150)
+    this.context.rotate(-0.2)
+    this.context.scale(1, 1)
     this.context.translate(5, -20)
     this.context.rotate(this._frame / 10 + 0.5)
     this.drawPath(
