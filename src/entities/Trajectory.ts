cc57cdd98c2a9d3106887ae9ab048349f4fb694f
Daniel Whiffing <danielwhiffing@gmail.com>
2023-09-11 20:51:17 -0400

    Adds better trajectory

--- src/entities/Trajectory.ts
+++ src/entities/Trajectory.ts
@@ -1,49 +1,41 @@
 import { emit, Pool } from 'kontra'
-import { GRAVITY } from '../constants'
+import { GRAVITY, GROUND_Y } from '../constants'
 import { Sprite } from './sprite'
 
 export class Trajectory {
   sprites: Pool
   angleSprite: AngleSprite
+  trajectorySprite: TrajectorySprite
   speed: number
   stage: number
   angle: number
 
   constructor(properties) {
-    // this.sprites = Pool({
-    //   create: () => new TrajectorySprite(properties),
-    //   maxSize: 10,
-    // })
+    this.trajectorySprite = new TrajectorySprite(properties)
     this.angleSprite = new AngleSprite(properties)
     this.stage = 0
     this.speed = properties.speed
     this.angle = properties.angle
-    // emit('delay', 'spawn', 20, () => this.spawn())
   }
 
-  // spawn() {
-  // @ts-ignore
-  // this.sprites.get()?.reset(this.speed, this.angle)
-  // emit('delay', 'spawn', 20, () => this.spawn())
-  // }
-
   update() {
-    // this.sprites.update()
+    this.trajectorySprite.opacity = this.stage === 2 ? 1 : 0
     this.angleSprite.opacity = this.stage > 0 ? 1 : 0
-    this.angleSprite.opacity = this.stage > 0 ? 1 : 0
-    if (this.stage === 0) {
-      return
-    }
+    if (this.stage === 0) return
 
     this.angleSprite.angle = this.angle
     this.angleSprite.speed = this.speed
     this.angleSprite.update()
+
+    this.trajectorySprite.angle = this.angle
+    this.trajectorySprite.speed = this.speed
+    this.trajectorySprite.update()
   }
 
   render() {
     if (this.stage === 0) return
-    // if (this.stage === 2) this.sprites.render()
     this.angleSprite.render()
+    this.trajectorySprite.render()
   }
 }
 class AngleSprite extends Sprite {
@@ -74,53 +66,39 @@ class AngleSprite extends Sprite {
     this.context.stroke()
   }
 }
+class TrajectorySprite extends Sprite {
+  constructor(properties = {}) {
+    super({
+      anchor: { x: 0, y: 0 },
+      ...properties,
+    })
+    this.opacity = 1
+  }
 
-// class TrajectorySprite extends Sprite {
-//   constructor(properties: any = {}) {
-//     super({
-//       anchor: { x: 0, y: 0 },
-//       ...properties,
-//       dx: 0,
-//       dy: 0,
-//       ddy: 0,
-//       opacity: 0,
-//     })
-//     this.particles = properties.particles
-//     this.initialX = properties.x
-//     this.initialY = properties.y
-//     this.maxY = properties.maxY
-//     this.context.fillStyle = '#fff'
-//     this.context.beginPath()
-//     this.opacity = 0
-//     this.ttl = 0
-//   }
+  update(dt?) {
+    super.update(dt)
+  }
 
-//   update(dt?) {
-//     super.update(dt)
-
-//     if (this.opacity <= 0) this.ttl = 0
-//     if (this.opacity === 0) return
-//     this.opacity -= 0.015
-//     if (this.y > this.maxY) {
-//       this.opacity = 0
-//       this.ttl = 0
-//     }
-//   }
-
-//   reset(speed, angle) {
-//     this.x = this.initialX
-//     this.y = this.initialY
-//     this.dx = speed * Math.cos(angle)
-//     this.dy = speed * Math.sin(angle)
-//     this.ddy = GRAVITY
-//     this.opacity = 0.7
-//   }
-
-//   draw() {
-//     const size = 40
-//     this.context.beginPath()
-//     this.context.fillStyle = `rgba(255,255,255,${this.opacity})`
-//     this.context.arc(0, 0, size / 2, 0, Math.PI * 2)
-//     this.context.fill()
-//   }
-// }
+  draw() {
+    const size = 8
+    let x = 0
+    let y = 0
+    let dx = this.speed * Math.cos(this.angle)
+    let dy = this.speed * Math.sin(this.angle)
+    let o = 0.4
+    for (let i = 0; i < 200; i++) {
+      x += dx
+      y += dy
+      dy += GRAVITY
+      // this.ddy = GRAVITY
+      if (y + 280 > GROUND_Y) break
+      if (i % 4 !== 0) continue
+      o -= 0.02
+      this.context.beginPath()
+      this.context.fillStyle = `rgba(255,255,255,${o})`
+      this.context.arc(x, y, size / 2, 0, Math.PI * 2)
+      this.context.closePath()
+      this.context.fill()
+    }
+  }
+}
